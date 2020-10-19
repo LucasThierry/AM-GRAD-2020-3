5b967ec015c8d7806d3117b947b85ea2e5c48645
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2020-10-18 23:35:49 -0300

    Scaler added and Dataset modified

--- SVM/SVM Scaler.py
+++ SVM/SVM Scaler.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedKFold
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 
 filename = 'glass 1.csv' # Features
 names = ['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba','Fe']
@@ -15,7 +17,7 @@ Y = pandas.read_csv(filename1, names=names1)
 print(Y)
 
 cv = RepeatedKFold(n_splits=5, n_repeats=10, random_state=1453) # Parametros de Validação
-clf = svm.SVC(kernel='sigmoid', C = 1.0) # Parametros do Modelo
+clf = make_pipeline(StandardScaler(), svm.SVC(kernel='linear', C = 1.0)) # Parametros do Modelo
 
 scores = cross_val_score(clf, X, Y.values.ravel(), scoring='accuracy', cv=cv) # Avaliação
 print("Accuracy: %.3f (%.3f)" % (scores.mean(), scores.std()))
