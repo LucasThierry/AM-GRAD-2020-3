71f4670ba95b09e23f93429d7242f4e7b254a89e
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2020-11-29 21:38:29 -0300

    MLP base updated

--- final_project/classifiers/mlp.py
+++ final_project/classifiers/mlp.py
@@ -15,7 +15,7 @@ class MLP(AbstractClassifier):
         """
         Evaluates the classifier.
         """
-        classifier = MLPClassifier(solver='adam', activation='relu', alpha=1e-5, hidden_layer_sizes=(5, 2), learning_rate_init=0.001, max_iter=200, random_state=1)
+        classifier = MLPClassifier(solver='adam', activation='relu', alpha=1e-5, hidden_layer_sizes=(10, 4), learning_rate='constant', learning_rate_init=0.001, max_iter=200, random_state=1)
         scores = self._build_scores(classifier)
         self._print_scores(scores)
 
