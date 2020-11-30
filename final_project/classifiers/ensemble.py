611f9c5fa27ebd88c5c1fde15bce191e33ab4aad
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2020-11-29 22:07:48 -0300

    Ensemble done

--- final_project/classifiers/ensemble.py
+++ final_project/classifiers/ensemble.py
@@ -3,8 +3,41 @@ Module for ensemble classifier.
 """
 
 from classifiers.abstract_classifier import AbstractClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import StackingClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
+
 
 
 class Ensemble(AbstractClassifier):
     """Class for ensemble classifier."""
-    pass
+
+    def evaluate(self):
+        """
+        Evaluates the classifier.
+        """
+        classifier = StackingClassifier(estimators=[('dt', DecisionTreeClassifier(max_depth=10, min_samples_leaf=1, splitter='random'))], 
+                stack_method='auto',
+                passthrough=False)
+        scores = self._build_scores(classifier)
+        self._print_scores(scores)
+
+    def grid_search(self, scores):
+        """
+        Runs the grid search of the classifier.
+        :param list[str] scores:
+        """
+        classifier = StackingClassifier(estimators=[('dt', DecisionTreeClassifier(max_depth=10, min_samples_leaf=1, splitter='random'))])
+        for score in scores:
+            self._perform_grid_search(classifier, score)
+
+
+    def _grid_parameters(self):
+        """
+        Returns the grid parameters.
+        :return dict:
+        """
+        return dict(
+                stack_method=['auto', 'predict_proba', 'decision_function', 'predict'],
+                passthrough=[False, True])
