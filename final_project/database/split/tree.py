79b5e1302da985f5ddf170789415f1a9b2953050
LucasThierry <34257897+LucasThierry@users.noreply.github.com>
2020-12-02 17:15:06 -0300

    Decision tree rkgk added

--- final_project/database/split/tree.py
+++ final_project/database/split/tree.py
@@ -3,13 +3,25 @@ import numpy as np
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 
-filename = 'aps_failure_training_set_attributes.csv'
-X = pandas.read_csv(filename)
+train1 = 'aps_failure_training_set_attributes.csv'
+Xp = pandas.read_csv(train1)
+X=Xp.fillna(Xp.mean())
 
-filename1 = 'aps_failure_training_set_classes.csv'
-Y = pandas.read_csv(filename1)
 
+train2 = 'aps_failure_training_set_classes.csv'
+Y = pandas.read_csv(train2)
+
+
+test1 = 'aps_failure_test_set_attributes.csv'
+Xtestp = pandas.read_csv(test1)
+Xtest=Xtestp.fillna(Xtestp.mean())
+
+
+test2 = 'aps_failure_test_set_classes.csv'
+Ytest = pandas.read_csv(test2)
 
 classifier = DecisionTreeClassifier(max_depth=10, min_samples_leaf=1, splitter='random')
 
 classifier.fit(X, Y)
+
+print(classifier.predict(Xtest))
